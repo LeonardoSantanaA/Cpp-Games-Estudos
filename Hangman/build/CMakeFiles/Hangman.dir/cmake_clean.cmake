81613e31277e73da81370ad87d82119a85aae95d
lekaum <lkdev496@gmail.com>
2023-03-07 20:42:19 -0300

    Hangman - 1

---
+++ Hangman/build/CMakeFiles/Hangman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hangman.dir/Hangman.cpp.o"
+  "CMakeFiles/Hangman.dir/Hangman.cpp.o.d"
+  "Hangman"
+  "Hangman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
