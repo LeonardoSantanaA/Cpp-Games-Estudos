a861607531c0888dac914b4a115bbd0cc7e743e5
lekaum <lkdev496@gmail.com>
2023-08-01 21:50:47 -0300

    concatenate & equal

--- Exercises/StringClass.cpp
+++ Exercises/StringClass.cpp
@@ -2,17 +2,17 @@
 
 class String{
   public:
-    String(char* str){
+    String(const char* str){
       this->str = (char*)str;
-      this->length = GetLength();
+      this->length = VerifyLength();
     }
+
     String():str((char*)"Hello"), length(5){}
+
     String(int length){
       str = new char[length];
+      this->length = length;
     }
-    //~String(){
-      //delete str;
-    //}
 
     inline int GetLength();
 
@@ -23,19 +23,29 @@ class String{
     }
 
     String operator+(String& otherString);
+    bool operator==(String& otherString);
 
+    int substr(const String& substr);
 
   private:
     char* str;
     int length = 0;
+
+    inline int VerifyLength();
 };
+
+inline int String::VerifyLength(){
+  int i = 0;
+  while(str[i] != '\0'){
+    i++;
+  }
+  return i;
+}
 
 int String::GetLength(){
   int i = 0;
   if(length == 0){
-    while(str[i] != '\0'){
-      i++;
-    }
+    i = VerifyLength();
   }else{
     return length;
   }
@@ -48,14 +58,8 @@ String String::operator+(String& otherString){
   int lastIndex = 0;
   String newString(newLength);
   
-  std::cout << *this;
-  std::cout << " + ";
-  std::cout << otherString;
-  std::cout << " = ";
-  std::cout << std::endl;
-
   for(int i = 0; i < newLength; i++){
-    if(i <= this->GetLength()){
+    if(this->str[i] != '\0'){
       newString.str[i] = this->str[i];
    }else{
       lastIndex = i;
@@ -66,23 +70,49 @@ String String::operator+(String& otherString){
   int index = 0;
   for(int i = lastIndex; i <= newLength; i++){
     newString.str[i] = otherString.str[index];
-    //std::cout << newString.str[i];
     index++;
   }
+
   return newString;
 }
 
 
+bool String::operator==(String& otherString){
+  if(this->GetLength() == otherString.GetLength()){
+    for(int i = 0; i < this->GetLength(); i++){
+      if((this->str[i] == otherString.str[i]) && this->str[i] != '\0'){
+        //std::cout << this->str[i] << " é igual de " << otherString.str[i] << std::endl;
+        continue;
+      }else{
+        return false;
+      }
+    }
+  }else{
+    return false;
+  }
+
+   return true; 
+}
+
+int String::substr(const String& substr){
+
+}
+
 int main( int argc , char **argv ){
-  String string((char*)"Holla");
-  String string2;
-  String newString = string + string2;
-  //std::cout << string;
+  String string((char*)"Hollba");
+  String string2 = "Lekaum ";
+  String string3 = "Santana";
+  String nome = string2 + string3;
+  if(string == string2){
+    std::cout << "is equal!" << std::endl;
+  }else{
+    std::cout << "inst equal!" << std::endl;
+  }
+  std::cout << nome;
   //std::cout << std::endl;
   //std::cout << string2;
   //std::cout << std::endl;
-  std::cout << "tamanho: " << newString.GetLength() << std::endl;
-  std::cout << newString;
+  //std::cout << string;
   return 0;
 }
 
