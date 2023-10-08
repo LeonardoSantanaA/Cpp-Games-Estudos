8cfb05136960a7abddf6efc4dd468d314af5db4a
Leonardo Santana <lkdev496@gmail.com>
2023-10-08 12:30:59 -0300

    sdl window surface

--- SDL2/Setup.cpp
+++ SDL2/Setup.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 #include <SDL.h>
 
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y);
+size_t GetIndex(SDL_Surface* noptrWindowSurface, int r, int c);
+
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_VIDEO);
-	std::cout << "Setup SDL2 with CMake" << std::endl;
-	system("pause");
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "Error SDL_Init Failed" << std::endl;
+		return 1;
+	}
+
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+	if (optrWindow == nullptr) {
+		std::cout << "Could not create window, got error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
+
+	uint32_t color = 0xFF0000;
+
+	SetPixel(noptrWindowSurface, color, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	SDL_UpdateWindowSurface(optrWindow);
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running) {
+		while (SDL_PollEvent(&sdlEvent)) {
+			switch (sdlEvent.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(optrWindow);
 	SDL_Quit();
+
 	return 0;
+}
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y) {
+	SDL_LockSurface(noptrWindowSurface);
+
+	uint32_t* pixels = (uint32_t*)noptrWindowSurface->pixels;
+
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+
+	pixels[index] = color;
+
+	SDL_UnlockSurface(noptrWindowSurface);
+}
+
+size_t GetIndex(SDL_Surface* noptrWindowSurface, int r, int c) {
+	return r * noptrWindowSurface->w + c;
 }
