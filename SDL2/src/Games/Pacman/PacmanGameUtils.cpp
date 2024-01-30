dd1f0afd1aac35d71d1f84c7d4d805e3965461f5
Leonardo Santana <lkdev496@gmail.com>
2024-01-30 09:02:22 -0300

    :floppy_disk:(#03): pacman - pacman utils

---
+++ SDL2/src/Games/Pacman/PacmanGameUtils.cpp
@@ -0,0 +1,55 @@
+#include "PacmanGameUtils.h"
+
+Vec2D GetMovementVector(PacmanMovement direction) {
+	if (direction == PACMAN_MOVEMENT_LEFT) {
+		return Vec2D(-1, 0);
+	}
+	else if (direction == PACMAN_MOVEMENT_DOWN) {
+		return Vec2D(0, 1);
+	}
+	else if (direction == PACMAN_MOVEMENT_RIGHT) {
+		return Vec2D(1, 0);
+	}
+	else if (direction == PACMAN_MOVEMENT_UP) {
+		return Vec2D(0, -1);
+	}
+
+	return Vec2D::Zero;
+}
+
+PacmanMovement GetOppositeDirection(PacmanMovement direction) {
+	if (direction == PACMAN_MOVEMENT_LEFT) {
+		return PACMAN_MOVEMENT_RIGHT;
+	}
+	else if (direction == PACMAN_MOVEMENT_DOWN) {
+		return PACMAN_MOVEMENT_UP;
+	}
+	else if (direction == PACMAN_MOVEMENT_RIGHT) {
+		return PACMAN_MOVEMENT_LEFT;
+	}
+	else if (direction == PACMAN_MOVEMENT_UP) {
+		return PACMAN_MOVEMENT_DOWN;
+	}
+
+	return PACMAN_MOVEMENT_NONE;
+}
+
+std::vector<PacmanMovement> GetPerpendicularMovements(PacmanMovement direction) {
+	std::vector<PacmanMovement> perperdincularDirections;
+
+	if (direction == PACMAN_MOVEMENT_DOWN || direction == PACMAN_MOVEMENT_UP) {
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_LEFT);
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_RIGHT);
+	}
+	else if (direction == PACMAN_MOVEMENT_RIGHT || direction == PACMAN_MOVEMENT_LEFT) {
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_UP);
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_DOWN);
+	}
+	else if (direction == PACMAN_MOVEMENT_NONE) {
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_LEFT);
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_RIGHT);
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_UP);
+		perperdincularDirections.push_back(PACMAN_MOVEMENT_DOWN);
+	}
+	return perperdincularDirections;
+}
---
+++ SDL2/src/Games/Pacman/PacmanGameUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../../Utils/Vec2D.h"
+#include <vector>
+
+enum PacmanMovement {
+	PACMAN_MOVEMENT_NONE = 0,
+	PACMAN_MOVEMENT_UP,
+	PACMAN_MOVEMENT_LEFT,
+	PACMAN_MOVEMENT_DOWN,
+	PACMAN_MOVEMENT_RIGHT
+};
+
+Vec2D GetMovementVector(PacmanMovement direction);
+PacmanMovement GetOppositeDirection(PacmanMovement direction);
+std::vector<PacmanMovement> GetPerpendicularMovements(PacmanMovement direction);
